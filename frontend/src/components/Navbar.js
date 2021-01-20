891b0c6a717de3009a181aa12f0973dd42b8bf2e
Nestor Gerardo Gonzalez Roque <nestor.ge222@gmail.com>
2021-01-20 09:56:41 -0600

    change on navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logout } from '../actions/auth'
 
 
-const Navbar = ({}) => {
+const Navbar = ({ logout, isAuthenticated}) => {
     const guestLinks=()=>{
+
+        <Fragment>
+            <li className= 'nav-item'd>
+                <Link className = 'nav-link' to='/login'>
+                    Login 
+                </Link>
+            </li>
+            <li>
+                <Link className = 'nav-link' to='/login'>
+                    Login 
+                </Link>
+                
+
+            </li>
+        </Fragment>
 
     };
     const authLinks =()=>{
@@ -14,33 +29,14 @@ const Navbar = ({}) => {
         return(
             <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand">Sistem</a>
+            <Link className="navbar-brand">Sistem</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" >Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" >Action</a>
-                    <a className="dropdown-item" >Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" >Something else here</a>
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled"  tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
+                    {isAuthenticated ? authLinks() : guestLinks()}
                 </ul>
               
             </div>
