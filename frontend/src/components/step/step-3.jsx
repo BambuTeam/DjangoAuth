dd7e32af84f241e9fd59b7cc320aafbd4721e1d3
Luis Enrique Concuá <51172708+econtrerasbcs@users.noreply.github.com>
2021-01-25 11:42:24 -0600

    Steps

--- frontend/src/components/step/step-3.jsx
+++ frontend/src/components/step/step-3.jsx
@@ -1,24 +1,97 @@
 import React from "react";
 import { StepComponentProps } from "react-step-builder";
 
-const Step3 = (props: StepComponentProps) => {
-    return (
-        <div>
-            <input
-                name="fname"
-                value={props.getState("fname", "")}
-                onChange={props.handleChange}
-            />
-            <input
-                name="lname"
-                value={props.getState("lname", "")}
-                onChange={props.handleChange}
-            />
 
-            <button onClick={props.prev}>Previous</button>
-            <button onClick={props.next}>Next</button>
-        </div>
+
+
+
+function Step3(props) {
+    return (
+        <div className="multisteps-form__panel" data-animation="slideHorz">
+                <div className="wizard-forms">
+                    <div className="inner pb-100 clearfix">
+                        <div className="form-content pera-content col-12">
+                            <div className="step-inner-content">
+                                <span className="step-no bottom-line">Step 3</span>
+                                <div className="step-progress float-right">
+                                    <span>3 of 3 completed</span>
+                                    <div className="step-progress-bar">
+                                        <div className="progress">
+                                            <div className="progress-bar" style={{width: '80%' }}></div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <h2>Pick up information</h2>
+                    <p>Please select the type of handle of your basket</p>
+                    <div className="budget-area">
+                        <p>Optimization and Accessibility</p>
+                        <div className="opti-list">
+                            <ul className="d-md-flex">
+                                <li className="bg-white active"><input type="checkbox" name="code_opti1" value="Pick Up"
+                                        defaultChecked />Pick Up</li>
+                                <li className="bg-white"><input type="checkbox" name="code_opti2"
+                                        value="Deliver to GOYA" />Deliver to GOYA</li>
+                                <li className="bg-white"><input type="checkbox" name="code_opti3"
+                                        value="Drop Off" />Drop Off</li>
+                                <li className="bg-white"><input type="checkbox" name="code_opti3" value="FedEx" />FedEx
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+
+                                <h2>Ordering</h2>
+                                <div className="form-inner-area">
+                                    <input type="text" name="custom_order" class="form-control valid" aria-invalid="false" placeholder="Description of Order"/>
+                                    <input type="date" name="order_date"/>
+                                    <input type="time" id="hour" name="hour" min="09:00" max="24:00" />
+                                    <div className="comment-box">
+                                        <div>
+                                            <p><i className="fas fa-shopping-basket"></i> Quantity Basket</p>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-4">
+                                                <input type="number" id="give_away" name="give_away" min="0" max="100"
+                                                    placeholder="Give Away (S)" />
+                                            </div>
+                                            <div className="col-4">
+                                                <input type="number" id="vip" name="vip" min="0" max="100"
+                                                    placeholder="V.I.P. (M)" />
+                                            </div>
+                                            <div className="col-4">
+                                                <input type="number" id="show_baskets" name="show_baskets" min="0" max="100"
+                                                    placeholder="Show Basket (L)" />                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="comment-box">
+                                        <p><i class="far fa-comment-alt"></i> Notes</p>
+                                        <textarea name="full_comments" placeholder="Write here"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/*
+                    <!-- /.inner -->*/}
+                    <div className="actions">
+                        <ul>
+                            
+                            <li><button className="js-btn-prev" title="BACK" onClick={props.prev}>BACK <i className="fa fa-arrow-left"></i></button></li>
+                            <li><button className="js-btn-prev" title="NEXT" onClick={props.next}>NEXT <i className="fa fa-arrow-right"></i></button></li>
+
+                            
+                           
+
+                            
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
     );
-};
+}
 
 export default Step3;
--- frontend/src/components/version-one.js
+++ frontend/src/components/version-one.js
@@ -1,19 +1,49 @@
-import React, { version } from "react";
+import React from "react";
 
 import { Steps, Step } from "react-step-builder";
 import Step1 from "./step/step-1";
 import Step2 from "./step/step-2";
 import Step3 from "./step/step-3";
 
+import svg from '../../src/assets/img/sideimg.svg';
+
+
 function VersionOne() {
   return (
-    <div className="App">
-      <Steps>
-        <Step component={Step1} />
-        <Step component={Step2} />
-        <Step component={Step3} />
-      </Steps>
-    </div>
+
+
+    <div className="wrapper">
+                <div className="steps-area steps-area-fixed">
+                    <div className="image-holder">
+
+                        <img src={svg} alt="" />
+                    </div>
+                    <div className="steps clearfix">
+                        <ul className="tablist multisteps-form__progress">
+                            <li className="multisteps-form__progress-btn js-active current">
+                                <span>1</span>
+                            </li>
+                            <li className="multisteps-form__progress-btn">
+                                <span>2</span>
+                            </li>
+                            <li className="multisteps-form__progress-btn">
+                                <span>3</span>
+                            </li>
+                        
+                        </ul>
+                    </div>
+                </div>
+                <div className="multisteps-form__form" >
+
+                
+                    <Steps>
+                        <Step component={Step1} />
+                        <Step component={Step2} />
+                        <Step component={Step3} />
+                    </Steps>
+                    </div>
+
+            </div>
   );
 }
 
