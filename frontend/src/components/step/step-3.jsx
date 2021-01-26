71cb48bb466a5377342ef4c61330d254e6b97a7a
Nestor Gerardo Gonzalez Roque <nestor.ge222@gmail.com>
2021-01-26 15:52:23 -0600

    algun cambio

--- frontend/src/components/step/step-3.jsx
+++ frontend/src/components/step/step-3.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import { StepComponentProps } from "react-step-builder";
+
+
+
+
 
 function Step3(props) {
+
+    const noshowMessage = ()=>{
+        var op  =   document.getElementById('miBoton')
+        op.style.display = "none";
+
+    }
+    const showMessage = ()=>{
+        var op  =   document.getElementById('miBoton')
+        op.style.display = "block";
+
+    }
+
+
+    const handleClic1 = ()=>{
+        setTimeout(noshowMessage, 1000);
+    }
+    const handleClic2 = ()=>{
+        setTimeout(showMessage, 1000);
+    }
+
+
     return (
         <div className="multisteps-form__panel" data-animation="slideHorz">
                 <div className="wizard-forms">
@@ -9,10 +35,7 @@ function Step3(props) {
                             <div className="step-inner-content">
                                 <span className="step-no bottom-line">Step 3</span>
                                 <div className="step-progress float-right">
-                                    <span>3 of 3 completed.</span>
-                                    <div className="svgmail" style={{width: '100%' }}>
-                                    <img  src="https://www.flaticon.com/svg/vstatic/svg/2089/2089181.svg?token=exp=1611687191~hmac=6b95fd08af6d2a83915108088b2d7c6c" />
-                                    </div>
+                                    <span>3 of 3 completed</span>
                                     <div className="step-progress-bar">
                                         <div className="progress">
                                             <div className="progress-bar" style={{width: '80%' }}></div>
@@ -23,11 +46,23 @@ function Step3(props) {
                                 <h2>Pick up information</h2>
                     <p>Please select the type of handle of your basket</p>
                     <div className="budget-area">
-                        
+                        <p>Optimization and Accessibility</p>
                         <div className="opti-list">
                             <ul className="d-md-flex">
-                                <button className="bg-white active"><input type="checkbox" name="code_opti1" value="Pick Up"
-                                        defaultChecked />Pick Up</button>
+
+                            <button onClick={handleClic1}> no un mensaje</button>
+                                <button onClick={handleClic2}> Show un mensaje</button>
+
+
+
+                                <button className="bg-white">
+                                    <input
+                                       type="radio"
+                                       name="code_opti1"
+                                       value="Pick Up"
+                                       defaultChecked
+                                    />
+                                        Pick Up</button>
                                 <button className="bg-white"><input type="checkbox" name="code_opti2"
                                         value="Deliver to GOYA" />Deliver to GOYA</button>
                                 <button className="bg-white"><input type="checkbox" name="code_opti3"
@@ -35,15 +70,15 @@ function Step3(props) {
                                 <button className="bg-white"><input type="checkbox" name="code_opti3" value="FedEx" />FedEx
                                 </button>
 
-                                
+
                             </ul>
-                            
+
                         </div>
                     </div>
 
 
                                 <h2>Ordering</h2>
-                                <div className="form-inner-area">
+                                <div className="form-inner-area" id="miBoton">
                                     <input type="text" name="custom_order" class="form-control valid" aria-invalid="false" placeholder="Description of Order"/>
                                     <input type="date" name="order_date"/>
                                     <input type="time" id="hour" name="hour" min="09:00" max="24:00" />
@@ -52,15 +87,21 @@ function Step3(props) {
                                             <p><i className="fas fa-shopping-basket"></i> Quantity Basket</p>
                                         </div>
                                         <div className="row">
-                                            <div className="col-lg-4 col-lg-4 col-sm-12">
-                                                <input type="number" id="give_away" name="give_away" min="0" max="100"
-                                                    placeholder="Give Away (S)" />
+                                            <div className="col-4">
+                                                <input
+                                                    type="number"
+                                                    id="give_away"
+                                                    name="give_away"
+                                                    min="0"
+                                                    max="100"
+                                                    placeholder="Give Away (S)"
+                                                />
                                             </div>
-                                            <div className="col-lg-4 col-lg-4 col-sm-12">
+                                            <div className="col-4">
                                                 <input type="number" id="vip" name="vip" min="0" max="100"
                                                     placeholder="V.I.P. (M)" />
                                             </div>
-                                            <div className="col-lg-4 col-lg-4 col-sm-12">
+                                            <div className="col-4">
                                                 <input type="number" id="show_baskets" name="show_baskets" min="0" max="100"
                                                     placeholder="Show Basket (L)" />                                            </div>
                                         </div>
@@ -79,20 +120,31 @@ function Step3(props) {
                     <!-- /.inner -->*/}
                     <div className="actions">
                         <ul>
-                            
+
                             <li><button className="js-btn-prev" title="BACK" onClick={props.prev}>BACK <i className="fa fa-arrow-left"></i></button></li>
                             <li><button className="js-btn-prev" title="NEXT" onClick={props.next}>NEXT <i className="fa fa-arrow-right"></i></button></li>
 
-                            
-                           
 
-                            
-                            
+
+
+
+
                         </ul>
                     </div>
                 </div>
             </div>
     );
 }
+
+
+
+
+    // Este enlace es necesario para hacer que `this` funcione en el callback    this.handleClick = this.handleClick.bind(this);  }
+
+
+
+
+
+
 
 export default Step3;
