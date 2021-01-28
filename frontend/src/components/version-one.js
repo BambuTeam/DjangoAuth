81a941c77c119f98bc90bf1da1f32d7aad559ac6
Nestor Gerardo Gonzalez Roque <nestor.ge222@gmail.com>
2021-01-28 15:00:13 -0600

    cambios en envio de datos usuaros

--- frontend/src/components/version-one.js
+++ frontend/src/components/version-one.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import { connect } from 'react-redux'
 import { Steps, Step } from "react-step-builder";
+import { Redirect } from 'react-router-dom'
 import Step1 from "./step/step-1";
 import Step2 from "./step/step-2";
 import Step3 from "./step/step-3";
@@ -10,10 +11,17 @@ import Step4 from "./step/step-4";
 import svg from '../../src/assets/img/sideimg.svg';
 
 
-function VersionOne() {
+function VersionOne(isAuthenticated, username, usermail) {
+
+
+    if (!isAuthenticated){
+        return <Redirect to = '/'/>
+    }
+
+    
+    var username = username;
+
   return (
-
-
     <div className="wrapper">
                 <div className="steps-area steps-area-fixed">
                     <div className="image-holder">
@@ -38,8 +46,8 @@ function VersionOne() {
                 <div className="multisteps-form__form" >
 
                 
-                    <Steps>
-                        <Step component={Step1} />
+                    <Steps >
+                        <Step component={Step1} username = {username}/>
                         <Step component={Step2} />
                         <Step component={Step3} />
                         <Step component={Step4} />
@@ -51,4 +59,21 @@ function VersionOne() {
   );
 }
 
-export default VersionOne;
+const mapStateToProps = state =>{
+    if(state.auth.user){
+        return({
+            isAuthenticated: state.auth.isAuthenticated,
+            unsername: state.auth.user.name,
+            usermail: state.auth.user.mail
+            
+        })
+    }
+    return({
+    isAuthenticated: state.auth.isAuthenticated,
+    unsername: null,
+    usermail: null
+    })
+}
+
+
+export default connect(mapStateToProps)(VersionOne);
--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const Home = ({ isAuthenticated }) => {
-
-    if (!isAuthenticated){
+const Home = ({ isAuthenticated, unsername, usermail }) => {
+        if (!isAuthenticated){
         return <Redirect to = '/'/>
     }
+
 
 
     return(
@@ -15,6 +15,14 @@ const Home = ({ isAuthenticated }) => {
             <h1 className='display-4'>Hello, world!</h1>
             <p className='lead'>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
             <hr className='my-4' />
+            <h1>User</h1>
+            <p></p>
+            <h2>{unsername}</h2>  
+            <p>{unsername}</p>
+            {console.log(unsername)}
+            
+
+
             <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
             <Link className='btn btn-primary btn-lg' to = 'login'>Learn more</Link>
         </div>
@@ -23,8 +31,21 @@ const Home = ({ isAuthenticated }) => {
 };
 
 
-const mapStateToProps = state =>({
-    isAuthenticated: state.auth.isAuthenticated
-})
+const mapStateToProps = state =>{
+    if(state.auth.user){
+        return({
+            isAuthenticated: state.auth.isAuthenticated,
+            unsername: state.auth.user.name,
+            usermail: state.auth.user.mail
+            
+        })
+    }
+    return({
+    isAuthenticated: state.auth.isAuthenticated,
+    unsername: null,
+    usermail: null
+    })
+}
+
 
 export default connect(mapStateToProps)(Home);
